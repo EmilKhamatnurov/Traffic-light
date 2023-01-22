38e7a9a586de009f16c62b3e416f8c4628ca3a94
EmilKhamatnurov <emil.khamatnurov@gmail.com>
2023-01-22 21:51:36 +0500

    Добавлены файлы программы
    html, css, js файлы

---
+++ index.js
@@ -0,0 +1,9 @@
+const btnNode = document.querySelector('.js-btn');
+const trafficLightNode = document.querySelector('.traffic-light');
+
+initLight(trafficLightNode);
+
+btnNode.addEventListener('click', function(){
+	switchLight(trafficLightNode);
+	WebGL2RenderingContext
+});
